762efb5bd15f3ae5424c7db5216b480c372af34d
Coeur <alexxisgobrin06@gmail.com>
2024-11-29 13:55:01 -0600

    fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:app/home_page.dart';
+import 'package:archie_bell/home_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
