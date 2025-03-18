f6b711a6648f48939c60830c7c843510570644f1
Gobrin <gobrin3707@saskpolytech.ca>
2025-03-18 16:30:22 -0600

    Refactor page naming scheme for the staff page.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:app/api/firebase_api.dart';
 import 'package:app/api/mongo_db.dart';
 import 'package:app/firebase_options.dart';
-import 'package:app/pages/staff_homepage.dart';
+import 'package:app/pages/staff_home_page.dart';
 import 'package:app/pages/legacy_home_page.dart';
 import 'package:app/pages/new_home_page.dart';
 import 'package:app/pages/list_page.dart';
